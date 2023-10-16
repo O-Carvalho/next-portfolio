9745413c4d43d643a1a49308a8bad8c2218a07fc
Matheus Carvalho <matheus.dev23@gmail.com>
2023-10-16 20:38:18 -0300

    Integracao Hygrapy

--- app/components/pages/home/work-experience/experience-item.tsx
+++ app/components/pages/home/work-experience/experience-item.tsx
@@ -1,17 +1,55 @@
+import { RichText } from "@/app/components/rich-text"
 import { TechBadge } from "@/app/components/tech-badge"
+import { WorkExperience } from "@/app/types/work-experience"
+import { differenceInMonths, differenceInYears, format } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
 import Image from "next/image"
 
-export const ExperienceItem = () => {
+type ExperienceItemProps = {
+    experience: WorkExperience
+}
+
+export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
+    const {
+        endDate,
+        companyLogo,
+        companyName,
+        companyUrl,
+        description,
+        role,
+        technologies
+    } = experience
+
+    const startDate = new Date(experience.startDate)
+
+    const formattedStartDate = format(startDate, 'MMM yyyy', { locale: ptBR})
+    const formattedEndDate = endDate ? format(new Date(endDate), 'MMM yyyy', { locale: ptBR}) : 'o momento'
+
+    const end = endDate ? new Date(endDate) : new Date()
+
+    const months = differenceInMonths(end, startDate)
+    const years = differenceInYears(end, startDate)
+    const monthsRemaining = months % 12
+
+    const formattedDuration = 
+        years > 0
+            ?`${years} ano${years > 1 ? 's' : ''}${
+                monthsRemaining > 0
+                    ? ` e ${monthsRemaining} mes${monthsRemaining > 1 ? 'es' : ''}`
+                    : ''
+            }`
+               : `${months} mes${months > 1 ? 'es' : ''}`
+
     return (
         <div className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
             <div className="flex flex-col items-center gap-4">
                 <div className="rounded-full border border-gray-500 p-0.5">
                     <Image 
-                        src="https://images.unsplash.com/photo-1696537768609-1cf03f53e893?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80"
+                        src={companyLogo.url}
                         width={40}
                         height={40}
                         className="rounded-full"
-                        alt="Logo da Empresa"                  
+                        alt={`Logo da Empresa ${companyName}`}                
                     />
                 </div>
 
@@ -21,28 +59,25 @@ export const ExperienceItem = () => {
             <div>
                 <div className="flex flex-col gap-2 text-sm sm:text-base">
                     <a 
-                    href=""
+                    href={companyUrl}
                     target="_blank"
                     className="text-gray-500 hover:text-emerald-500 transition-colors"
                     >
-                        @ PSI Energy
+                        {companyName}
                     </a>
-                    <h4 className="text-gray-300">Desenvolvedor Front-End</h4>
+                    <h4 className="text-gray-300">{role}</h4>
                     <span className="text-gray-500">
-                        mar 2019 ° O momento ° (4 anos)
+                        {formattedStartDate} ° {formattedEndDate} ° ({formattedDuration})
                     </span>
-                    <p className="text-gray-400">
-                        Desenvolvimento e manutenção de websites, planejamento e controle de produção
-                    </p>
+                    <div className="text-gray-400">
+                        <RichText content={description.raw}/>
+                    </div>
                 </div>
                 <p className="text-gray-400 text-sm mb-3 font-semibold">Competências</p>
                 <div className="flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8">
-                    <TechBadge name="React" />
-                    <TechBadge name="React" />
-                    <TechBadge name="React" />
-                    <TechBadge name="React" />
-                    <TechBadge name="React" />
-                    <TechBadge name="React" />
+                    {technologies.map(tech => (
+                        <TechBadge key={`experience-${companyName}-tech-${tech.name}`} name={tech.name} />
+                    ))}
                 </div>
             </div>
         </div>
--- app/components/pages/known-techs/index.tsx
+++ app/components/pages/known-techs/index.tsx
@@ -1,19 +1,22 @@
-import { TbBrandNextjs } from "react-icons/tb"
+
 import { SectionTitle } from "../../section-title"
 import { KnownTech } from "./known-tech"
+import { KnownTech as IKnownTech } from "@/app/types/projects"
 
-export const KnownTechs = () => {
+type KnownTechProps = {
+    techs: IKnownTech[]
+}
+
+export const KnownTechs = ({techs }: KnownTechProps) => {
     return (
         <section className="container py-16">
             <SectionTitle subtitle="competências" title="Conhecimentos"/>
 
             <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
-            {Array.from({ length: 8 }).map((_, index) => (
-                <KnownTech key={index} tech={{
-                    icon: <TbBrandNextjs />,
-                    name: 'Next.js',
-                    startDate: '2022-09-01'
-                }}/>
+            {techs?.map((tech) => (
+                <KnownTech key={tech.name} tech={tech}
+                
+                />
             ))}
             </div>
         </section>
