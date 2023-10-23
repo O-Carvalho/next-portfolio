f1716e61035b771eb0b5b7827f01f906c9f638b7
Matheus Carvalho <matheus.dev23@gmail.com>
2023-10-23 20:54:32 -0300

    Animações com Framer Motion

--- app/components/pages/projects/page-introduction/index.tsx
+++ app/components/pages/projects/page-introduction/index.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import { Link } from "@/app/components/link"
 import { SectionTitle } from "@/app/components/section-title"
 import { HiArrowNarrowLeft } from "react-icons/hi"
+import { motion } from "framer-motion"
 
 export const PageIntroduction = () => {
     return (
@@ -10,8 +13,14 @@ export const PageIntroduction = () => {
                 title="Meus Projetos"
                 className="text-center items-center [&>h3]:text-4xl"
             />
-            <div className="flex flex-col items-center">
-                <p className="text-gray-400 text-center max-w-[640px] my-6 text-sm sm:text-base">
+            <motion.div 
+                className="flex flex-col items-center"
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 100 }}
+                transition={{ duration: 0.6 }}
+                >
+                <p className="text-gray-400 text-center max-w-[640px] my-6 text-sm sm:text-base"            >
                     Aqui você pode ver alguns trabalhos que desenvolvi. Navegue à vontade e explore os projetos para ver como
                     foram criados, as tecnologias utilizadas e as funcionalidades implementadas.
                 </p>
@@ -19,7 +28,7 @@ export const PageIntroduction = () => {
                     <HiArrowNarrowLeft size={20} />
                     Voltar para Home
                 </Link>
-            </div>
+            </motion.div>
         </section>
     )
 }
